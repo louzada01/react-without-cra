8db04a6b49bd0fd100b0a757674425c5bb1bf379
Louzada <silvalouzada@gmail.com>
2020-04-11 04:08:03 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Logo from "./logo.jpg";
+import "./App.css";
+
+export default function App() {
+  return (
+      <section>
+        <h1>Hello</h1>
+        <img src={Logo} alt="Logo" />
+        <h6>Edit to `./src/App.js`</h6>
+      </section>
+  );
+}
